0ce5e111066bdff96894b4b2e7d4a9ad7b61d973
VisionTr2021 <zahraa.ahm20@outlook.com>
2021-08-03 23:55:53 +0700

    init

---
+++ app/n-closet/lib/constants/app.dart
@@ -0,0 +1,13 @@
+const String URL = 'https://www.n-closet.com';
+
+const String CONSUMER_KEY = 'ck_ebb445014a0426cc79c8bf8b79e96eed0fe46ef6';
+
+const String CONSUMER_SECRET = 'cs_c0cc80d70b929498b61d52306438f1b5ad9ad661';
+
+const String REST_PREFIX = 'wp-json';
+
+const String defaultLanguage = 'en';
+
+const List<String> languageSupport = ['en', 'ar'];
+
+const String googleClientId = '295269595518-e7s01ueadskq7sbg2k4g4dfnefpmd7vt.apps.googleusercontent.com';
